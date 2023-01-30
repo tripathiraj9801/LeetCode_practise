32dbb028c734d7c8947107378e5ae81725a91ea3
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-01-30 08:07:38 +0530

    30 January 23 daily

---
+++ Tribonnaci.py
@@ -0,0 +1,13 @@
+class Solution:
+    dict1 = {0:0,1:1,2:1}
+
+    
+    def tribonacci(self, n: int) -> int:        
+        if n<3:
+            return self.dict1[n]
+
+        elif n in self.dict1:
+            return self.dict1[n]
+
+        self.dict1[n] = self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1)
+        return self.dict1[n]
