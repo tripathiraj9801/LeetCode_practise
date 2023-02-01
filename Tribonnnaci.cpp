43150f9ea4d8b5c8e2eeb6950a89567fc1e7a6e1
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-02-01 08:04:22 +0530

    30 January 23 daily

--- Tribonnnaci.cpp
+++ Tribonnnaci.cpp
@@ -1,24 +1,39 @@
-// Problem Link https://leetcode.com/problems/n-th-tribonacci-number/description/
+// // Problem Link https://leetcode.com/problems/n-th-tribonacci-number/description/
 
-class Solution
+// class Solution
+// {
+// public:
+//     int tribonacci(int num)
+//     {
+//         int num_1 = 0, num_2 = 1, num_3 = 1, num_n;
+
+//         if (num < 2)
+//             return num;
+//         else if (num == 2)
+//             return 1;
+
+//         for (int count = 3; count <= num; count++)
+//         {
+//             num_n = num_1 + num_2 + num_3;
+//             num_1 = num_2;
+//             num_2 = num_3;
+//             num_3 = num_n;
+//         }
+//         return num_n;
+//     }
+// };
+
+#include <iostream>
+using namespace std;
+
+int print_number()
 {
-public:
-    int tribonacci(int num)
-    {
-        int num_1 = 0, num_2 = 1, num_3 = 1, num_n;
+    cout << 1;
 
-        if (num < 2)
-            return num;
-        else if (num == 2)
-            return 1;
+    print_number();
+}
 
-        for (int count = 3; count <= num; count++)
-        {
-            num_n = num_1 + num_2 + num_3;
-            num_1 = num_2;
-            num_2 = num_3;
-            num_3 = num_n;
-        }
-        return num_n;
-    }
-};
+int main()
+{
+    print_number();
+}
