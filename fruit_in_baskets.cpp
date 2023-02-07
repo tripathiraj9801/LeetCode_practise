5e21648c24c191dc818c5cdb3f3fcfa4301cd4c6
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-02-07 08:49:19 +0530

    07 February 23 daily

---
+++ fruit_in_baskets.cpp
@@ -0,0 +1,31 @@
+// Problem Link https://leetcode.com/problems/fruit-into-baskets/description/
+
+class Solution
+{
+public:
+    int totalFruit(vector<int> &fruits)
+    {
+        map<int, int> basket;
+        int mx = 0;
+
+        int i = 0, j = 0;
+        int n = fruits.size();
+        while (i < n)
+        {
+            ++basket[fruits[i]];
+
+            while (j < n && (int)basket.size() > 2)
+            {
+                if (basket[fruits[j]] == 1)
+                    basket.erase(fruits[j]);
+                else
+                    --basket[fruits[j]];
+                ++j;
+            }
+
+            mx = max(mx, i - j + 1);
+            ++i;
+        }
+        return mx;
+    }
+};
