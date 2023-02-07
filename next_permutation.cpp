78e53ae026f4b4303451dea07854a624fb3ba218
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-02-07 08:53:12 +0530

    medium

---
+++ next_permutation.cpp
@@ -0,0 +1,10 @@
+// Problem Link https://leetcode.com/problems/next-permutation/description/
+
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &nums)
+    {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
