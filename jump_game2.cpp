80faec9a1bd2f6b66371dce419f349bd4ee06569
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-02-08 10:07:42 +0530

    8 feb 23 daily

---
+++ jump_game2.cpp
@@ -0,0 +1,23 @@
+// Problem Link https://leetcode.com/problems/jump-game-ii/description/
+
+class Solution
+{
+public:
+    int jump(vector<int> &nums)
+    {
+        int ans = 0;
+        int n = nums.size();
+        int currEnd = 0;
+        int currFarthest = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            currFarthest = max(currFarthest, i + nums[i]);
+            if (i == currEnd)
+            {
+                ans++;
+                currEnd = currFarthest;
+            }
+        }
+        return ans;
+    }
+};
