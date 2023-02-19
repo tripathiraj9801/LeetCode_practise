3d0f998dec8fe362802543b39cf099f40b784a2c
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-02-19 09:35:24 +0530

    tree medium;

---
+++ flatten_nested_list.cpp
@@ -0,0 +1,31 @@
+class NestedIterator
+{
+public:
+    stack<int> st;
+    NestedIterator(vector<NestedInteger> &nestedList)
+    {
+        ListToInteger(nestedList);
+        void ListToInteger(vector<NestedInteger> & nestedList)
+        {
+            for (int i = nestedList.size() - 1; i >= 0; i--)
+            {
+                if (nestedList[i].isInteger())
+
+                    st.push(nestedList[i].getInteger());
+                else
+                    ListToInteger(nestedList[i].getList());
+            }
+        }
+
+        int next()
+        {
+            int temp = st.top();
+            st.pop();
+            return temp;
+        }
+
+        bool hasNext()
+        {
+            return !st.empty();
+        }
+    };
