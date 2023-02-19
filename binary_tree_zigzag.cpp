92c1b0971e3511691b3849d8a20afbc1f9fa7211
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-02-19 09:04:05 +0530

    optimal code sol

--- binary_tree_zigzag.cpp
+++ binary_tree_zigzag.cpp
@@ -11,49 +11,82 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution
 {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode *root)
     {
-        vector<vector<int>> res;
+        vector<vector<int>> ans;
         if (!root)
-            return res;
-
+            return ans;
         queue<TreeNode *> q;
         q.push(root);
-
-        int level = 1;
+        int i = 0;
         while (!q.empty())
         {
-            vector<int> res1;
-            int n = q.size();
-
-            for (int i = 0; i < n; i++)
+            int sz = q.size();
+            vector<int> v;
+            while (sz--)
             {
-                TreeNode *temp = q.front();
+                TreeNode *f = q.front();
+                v.push_back(q.front()->val);
                 q.pop();
-                res1.push_back(temp->val);
-                cout << temp->val << " ";
-                if (level % 2 == 0)
-                {
-                    if (temp->left)
-                        q.push(temp->left);
-                    if (temp->right)
-                        q.push(temp->right);
-                }
-                else
-                {
-                    if (temp->right)
-                        q.push(temp->right);
-                    if (temp->left)
-                        q.push(temp->left);
-                }
+                if (f->left)
+                    q.push(f->left);
+                if (f->right)
+                    q.push(f->right);
             }
-
-            level++;
-            res.push_back(res1);
+            if (i++ % 2)
+                reverse(v.begin(), v.end());
+            ans.push_back(v);
         }
-        return res;
+        return ans;
     }
-};
+    // };
+    // class Solution
+    // {
+    // public:
+    //     vector<vector<int>> zigzagLevelOrder(TreeNode *root)
+    //     {
+    //         vector<vector<int>> res;
+    //         if (!root)
+    //             return res;
+
+    //         queue<TreeNode *> q;
+    //         q.push(root);
+
+    //         int level = 1;
+    //         while (!q.empty())
+    //         {
+    //             vector<int> res1;
+    //             int n = q.size();
+
+    //             for (int i = 0; i < n; i++)
+    //             {
+    //                 TreeNode *temp = q.front();
+    //                 q.pop();
+    //                 res1.push_back(temp->val);
+    //                 cout << temp->val << " ";
+    //                 if (level % 2 == 0)
+    //                 {
+    //                     if (temp->left)
+    //                         q.push(temp->left);
+    //                     if (temp->right)
+    //                         q.push(temp->right);
+    //                 }
+    //                 else
+    //                 {
+    //                     if (temp->right)
+    //                         q.push(temp->right);
+    //                     if (temp->left)
+    //                         q.push(temp->left);
+    //                 }
+    //             }
+
+    //             level++;
+    //             res.push_back(res1);
+    //         }
+    //         return res;
+    //     }
+    // };
