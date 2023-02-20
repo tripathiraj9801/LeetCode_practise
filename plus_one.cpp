a7476d4b45a2eb375b9c78bdeab22272cb68b645
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-02-20 08:43:51 +0530

    easy

--- plus_one.cpp
+++ plus_one.cpp
@@ -1,1 +1,21 @@
 // Problem Link https://leetcode.com/problems/plus-one/description/
+
+class Solution
+{
+public:
+    vector<int> plusOne(vector<int> &digits)
+    {
+        int carr = 1;
+        for (int i = digits.size() - 1; i >= 0; i--)
+        {
+            int sum = carr + digits[i];
+            digits[i] = sum % 10;
+            carr = sum / 10;
+        }
+
+        if (carr)
+            digits.insert(digits.begin(), carr);
+
+        return digits;
+    }
+};
