bfa9adbfb942501704632467da11e46dc0af26c2
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-02-20 08:17:56 +0530

    20 Feb 23 daily

---
+++ search_insertpos.cpp
@@ -0,0 +1,27 @@
+// Problem Link https://leetcode.com/problems/search-insert-position/description/
+
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        int low = 0;
+        int high = nums.size() - 1;
+        int mid = 0;
+        while (low <= high)
+        {
+            mid = (low + high) / 2;
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] > target)
+            {
+                high = mid - 1;
+            }
+            else
+                low = mid + 1;
+        }
+        return high + 1;
+    }
+};
