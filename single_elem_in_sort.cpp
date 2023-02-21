c9518f4dd4e9a320adebffa04d9159a28cdff163
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-02-21 08:12:52 +0530

    21 Feb 23 daily

---
+++ single_elem_in_sort.cpp
@@ -0,0 +1,20 @@
+// Problem Link https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+
+class Solution
+{
+public:
+    int singleNonDuplicate(vector<int> &nums)
+    {
+
+        unordered_map<int, int> mpp;
+        for (auto i : nums)
+        {
+            mpp[i]++;
+        }
+        for (auto it : mpp)
+            if (it.second == 1)
+                return it.first;
+
+        return -1;
+    }
+};
