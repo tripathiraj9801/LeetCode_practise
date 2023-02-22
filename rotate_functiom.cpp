719c8ff696f31d84c514eadb61e44c3a847de233
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-02-22 19:46:45 +0530

    dp medium

---
+++ rotate_functiom.cpp
@@ -0,0 +1,24 @@
+// Problem Link https://leetcode.com/problems/rotate-function/
+
+class Solution
+{
+public:
+    int maxRotateFunction(vector<int> &nums)
+    {
+        int ans = INT_MIN;
+        int n = nums.size();
+        int sum = 0;
+        int sum2 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += nums[i];
+            sum2 += nums[i] * i;
+        }
+        for (int i = n - 1; i >= 0; i--)
+        {
+            sum2 = sum2 - (nums[i] * (n - 1)) + (sum - nums[i]);
+            ans = max(ans, sum2);
+        }
+        return ans;
+    }
+};
