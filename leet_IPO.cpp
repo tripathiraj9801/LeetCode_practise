4e770068bc03c5499cbc0db7a20a015dc51eeaf7
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-02-23 11:19:05 +0530

    2 attempt

--- leet_IPO.cpp
+++ leet_IPO.cpp
@@ -30,3 +30,42 @@ public:
         return w;
     }
 };
+
+// Solution II
+
+class Solution
+{
+public:
+    int findMaximizedCapital(int k, int w, vector<int> &pft, vector<int> &c)
+    {
+        int n = pft.size();
+        vector<pair<int, int>> p(n);
+        set<int> s;
+
+        for (int i = 0; i < n; i++)
+            p[i] = {c[i], pft[i]};
+        sort(p.begin(), p.end());
+        reverse(p.begin(), p.end());
+
+        while (p.size() and p.back().first <= w)
+        {
+            s.insert(p.back().second);
+            p.pop_back();
+        }
+
+        while (s.size() and k)
+        {
+            w += *(--s.end());
+            s.erase(--s.end());
+            k--;
+
+            while (p.size() and p.back().first <= w)
+            {
+                s.insert(p.back().second);
+                p.pop_back();
+            }
+        }
+
+        return w;
+    }
+};
