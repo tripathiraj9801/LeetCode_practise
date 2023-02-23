b130d4f25e545f821d403a24b67f092d0aeb6a48
tripathiraj9801 <tripathirajeev9801@gmail.com>
2023-02-23 11:03:43 +0530

    23 Feb 23 daily 1 attempt

---
+++ leet_IPO.cpp
@@ -0,0 +1,32 @@
+// Problem Link https://leetcode.com/problems/ipo/
+
+class Solution
+{
+public:
+    int findMaximizedCapital(int k, int w, vector<int> &pft, vector<int> &c)
+    {
+        int n = pft.size();
+        vector<pair<int, int>> p(n);
+        set<int> s;
+
+        for (int i = 0; i < n; i++)
+            p[i] = {c[i], pft[i]};
+        sort(p.begin(), p.end());
+        reverse(p.begin(), p.end());
+
+        while (s.size() and k)
+        {
+            w += *(--s.end());
+            s.erase(--s.end());
+            k--;
+
+            while (p.size() and p.back().first <= w)
+            {
+                s.insert(p.back().second);
+                p.pop_back();
+            }
+        }
+
+        return w;
+    }
+};
